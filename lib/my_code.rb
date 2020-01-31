fc5a03914e470ee7d7dbe390e8c45293b533f109
Alessandro Duterte <alexduterte@gmail.com>
2020-01-31 01:43:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,13 @@ end
 def map_to_no_change(array)
   array
 end
+
+def map_to_double(array)
+  result = []
+  i=0
+  while i < array.length
+    result << array[i] * 2
+    i += 1
+  end
+  result
+end
